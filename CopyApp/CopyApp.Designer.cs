b922564a2cd018153ec6d173c6f97473737ef1b3
BartekPacocha <bartekpacocha@gmail.com>
2018-05-17 00:16:40 +0200

    first commit

---
+++ CopyApp/CopyApp.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CopyApp
+{
+    partial class CopyApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Execute = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btOpenFolder = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 163);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Execute
+            // 
+            this.Execute.Location = new System.Drawing.Point(23, 51);
+            this.Execute.Margin = new System.Windows.Forms.Padding(2);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(83, 22);
+            this.Execute.TabIndex = 1;
+            this.Execute.Text = "Execute All";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(21, 123);
+            this.pathLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(32, 13);
+            this.pathLabel.TabIndex = 2;
+            this.pathLabel.Text = "Path:";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(57, 120);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(198, 20);
+            this.tbPath.TabIndex = 3;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(23, 78);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(82, 23);
+            this.btDelete.TabIndex = 4;
+            this.btDelete.Text = "Delete old file";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btOpenFolder
+            // 
+            this.btOpenFolder.Location = new System.Drawing.Point(260, 120);
+            this.btOpenFolder.Name = "btOpenFolder";
+            this.btOpenFolder.Size = new System.Drawing.Size(18, 20);
+            this.btOpenFolder.TabIndex = 5;
+            this.btOpenFolder.UseVisualStyleBackColor = true;
+            this.btOpenFolder.Visible = false;
+            this.btOpenFolder.Click += new System.EventHandler(this.btOpenFolder_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(302, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cleanToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // cleanToolStripMenuItem
+            // 
+            this.cleanToolStripMenuItem.Name = "cleanToolStripMenuItem";
+            this.cleanToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cleanToolStripMenuItem.Text = "Clear";
+            this.cleanToolStripMenuItem.Click += new System.EventHandler(this.cleanToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // CopyApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 193);
+            this.Controls.Add(this.btOpenFolder);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "CopyApp";
+            this.Text = "CopyApp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btOpenFolder;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+    }
+}
+
---
+++ CopyApp/CopyApp.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyApp
+{
+    public partial class CopyApp : Form
+    {
+        public CopyApp()
+        {
+            InitializeComponent();
+        }
+
+        public int CreateDir(string path)
+        {
+            int status = 0;
+
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    label1.Text = "Path already exist";
+                    return 0;
+                }
+
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                label1.Text = "The directory was created successfully";
+                status = 1;
+            }
+            catch
+            {
+                label1.Text = "created failed";
+            }
+
+            return status;
+        }
+
+        public void CopyFile(string sourceFile, string destFile)
+        {
+            try
+            {
+                System.IO.File.Copy(sourceFile, destFile, true);
+            }
+            catch(FileNotFoundException e)
+            {
+                label1.Text = e.ToString();
+            }
+        }
+
+        public void DeleteOlder( string path )
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (di.CreationTime < DateTime.Now.AddDays(-2)) di.Delete();
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            string fileName = @"ML00000000";
+            string sourcePath = @"C:\Users\Cojman\Documents\KONAMI\Pro Evolution Soccer 2016\save";
+            string targetPath = @"F:\pes16\";
+            string path = targetPath + DateTime.Now.ToString("yyyy-MM-dd-HHmm");
+
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(path, fileName);
+
+            tbPath.Text = path;
+            if (CreateDir(path) > 0)
+            {
+                CopyFile(sourceFile, destFile);
+                label1.Visible = true;
+            }
+            btOpenFolder.Visible = true;
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            string path = @"F:\pes16\";
+            DeleteOlder(path);
+        }
+
+        private void btOpenFolder_Click(object sender, EventArgs e)
+        {
+            string path = tbPath.Text;
+            System.Diagnostics.Process.Start(path);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void cleanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Action<Control.ControlCollection> func = null;
+            func = (controls) =>
+            {
+                foreach (Control control in controls)
+                    if (control is TextBox)
+                        (control as TextBox).Clear();
+                    else
+                        func(control.Controls);
+            };
+            func(Controls);
+
+            label1.Text = null;
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
